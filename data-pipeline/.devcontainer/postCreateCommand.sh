fa7aa269e98789bb5236e5342f7697c3d19081b6
uuboyscy <aegis12321@gmail.com>
2024-10-12 11:35:40 +0800

    Some changes

--- data-pipeline/.devcontainer/postCreateCommand.sh
+++ data-pipeline/.devcontainer/postCreateCommand.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
 
-pip install pipenv \
-&& pipenv lock --dev \
-&& pipenv requirements --dev > requirements.txt \
-&& pip install -r requirements.txt
+# pip install pipenv \
+# && pipenv lock --dev \
+# && pipenv requirements --dev > requirements.txt \
+# && pip install -r requirements.txt
 
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="xiong-chiamiov-plus"/' /root/.zshrc \
 && sed -i '/HIST_STAMPS="mm\/dd\/yyyy"/s/^#*//;s/mm\/dd\/yyyy/yyyy-mm-dd/' /root/.zshrc
